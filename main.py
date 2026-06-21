a3392ffe2bbc192beef8b58e929b64867a327a1b
Kartik Kaushik <kartikgeu@gmail.com>
2026-06-21 16:31:17 +0530

    added ReAct search agent with Tavily

--- main.py
+++ main.py
@@ -3,36 +3,40 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from schemas import AgentResponse
 
-tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
 
+
+
+
+llm = ChatOpenAI(model="gpt-4o-mini")
+
+tools = [TavilySearch]
 
 agent = create_agent(
     model=llm,
-    tools=tools,
-    response_format=AgentResponse,
+    tools=tools
 )
 
 
 def main():
+    print("Hello from langchain-course!")
+
     result = agent.invoke(
         {
             "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
+                HumanMessage(
+                    content="Search the real time job postings for an AI engineer using langchain in the banglore on linkedin and list thier details"
+                )
             ]
         }
     )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
+
+    print(result)
 
 
 if __name__ == "__main__":
